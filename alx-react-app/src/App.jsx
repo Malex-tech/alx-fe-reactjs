9c7e514a53ae2efd8a698e761abad62b550b5e1a
Alex Alexander <raregemalex@gmail.com>
2025-08-30 22:48:00 +0100

    Added UserProfile component using props and integrated into App

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,7 +2,8 @@ import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
-import './App.css'
+import './App.css';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+      <UserProfile name="John Doe" age={30} bio="A software developer from NY." />
     </>
   );
 }
