15eaea1629804c1a0730b9befbcb5722519669c4
Alex Alexander <raregemalex@gmail.com>
2025-09-14 08:55:44 +0100

    Add init Src components recipeStore.js

---
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -0,0 +1,15 @@
+// src/components/recipeStore.js
+import { create } from "zustand";
+
+export const useRecipeStore = create((set) => ({
+  recipes: [],
+  addRecipe: (recipe) =>
+    set((state) => ({
+      recipes: [...state.recipes, recipe],
+    })),
+  removeRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((r) => r.id !== id),
+    })),
+  clearRecipes: () => set({ recipes: [] }),
+}));
