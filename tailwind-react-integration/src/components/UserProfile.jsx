fbc52a59d2db2055d8491bf0d8473f4aa46c2690
Alex Alexander <raregemalex@gmail.com>
2025-10-02 14:35:12 +0100

    Add modified src/components/UserProfile.jsx Responsive image sizing

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -4,7 +4,7 @@ function UserProfile() {
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full w-24 h-24 md:w-36 md:h-36 mx-auto transform transition-transform duration-300 ease-in-out hover:scale-110"
+        className="rounded-full w-20 h-20 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto transform transition-transform duration-300 ease-in-out hover:scale-110"
       />
       <h1 className="text-lg md:text-xl text-blue-800 my-3 md:my-4 font-semibold transition-colors duration-300 ease-in-out hover:text-blue-500">
         John Doe
@@ -17,4 +17,5 @@ function UserProfile() {
 }
 
 export default UserProfile;
+
 
